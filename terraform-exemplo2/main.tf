ac0340b1e2ce2ff5b1bfa18b48f429f71482a281
andersonluix <andersonlbsilva@gmail.com>
2022-12-04 00:28:08 -0200

    ajusta arquivos

--- terraform-exemplo2/main.tf
+++ terraform-exemplo2/main.tf
@@ -6,14 +6,14 @@ terraform {
   }
 
    backend "gcs" {
-    bucket  = "barberoterraform"
+    bucket  = "teste_terraform"
     prefix  = "terraform/state"
   }
   
 }
 
 provider "google" {
-  project = "barbero-devops-iac"
+  project = "projeto-kubernetes-dio-368818"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
@@ -23,7 +23,7 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "cloudbbuildterraform"
   machine_type = "f1-micro"
   tags = ["prod"]
 
